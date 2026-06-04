15c21d40fade8bb641b64687362596cd0e6f8ff0
Axel <axelmychro@gmail.com>
2026-06-04 15:07:09 +0700

    Tebak Kata: Output the length of pilihKataAcak as underscores

--- 02-tebak-kata/main.cpp
+++ 02-tebak-kata/main.cpp
@@ -1,7 +1,24 @@
+#include <iostream>
 #include "lib_tebak_kata.h"
 using namespace std;
 
 int main()
 {
+		/*
+		 * INISIALISASI
+		 */
+		KataGame game;
+		game.kata_asli = { "lompat",	"gaya",		  "tanaman",  "bibi",
+						   "pencarian", "kesempatan", "hamparan", "bersyukur",
+						   "asumsi",	"rahang",	  "pingsan",  "saksi",
+						   "rekaman",	"narasumber", "simulasi" };
+
+		//cout << time(0) << endl;
+		string kata_rahasia = game.pilihKataAcak();
+
+		for (int i = 0; i < kata_rahasia.size(); ++i)
+				cout << "_";
+		cout << endl;
+
 		return 0;
 }
